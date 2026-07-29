e436326acdd2f872e819ab861c245e0f493fa3d3
B Hymavathi <s180805@rguktsklm.ac.in>
2026-07-29 11:05:52 +0530

    changes 2

--- App/agents/ta_agent.py
+++ App/agents/ta_agent.py
@@ -1,11 +1,34 @@
 # pyrefly: ignore [missing-import]
 from langchain_groq import ChatGroq
+from langchain_core.tools import tool
+from langchain_community.tools import DuckDuckGoSearchRun
 
 import os
 # pyrefly: ignore [missing-import]
 from dotenv import load_dotenv
+from App.services.rag_service import EmbeddingService
 
 load_dotenv()
+
+@tool
+def retriever(query:str)->list:
+    """
+    This tool is used to retrieve data from a pdf which answer questions,
+    on C programming language. This tool retrieves the excat curriculum taught in DSTAM.
+    Use this tool to gain information for C programming curriculum of DSATM.
+    
+    Query(str): The query given / enhanced by the AI Agent
+
+    Output"
+    Chunks(list): The chunks retrieved from vectorDB
+    """
+    embedding_service = EmbeddingService()
+    context = embedding_service.retrieve_from_pdf(query)
+    return context
+
+
+web_search = DuckDuckGoSearchRun()
+
 
 class TeachingAssistantAgent:
     def __init__(self):
@@ -20,5 +43,12 @@ class TeachingAssistantAgent:
         pass
 
 if __name__ ==  "__main__":
-    query = input("Enter your query: ")
-    print(run_llm(query))
+    # query = input("Enter your query: ")
+    # print(run_llm(query))
+
+    chunk = retriever.invoke("what are pointer?")
+    print("\n\n CHUNKS RETRIEVED")
+    print(chunk)
+
+    search = web_search.invoke("LA Olympics 2028")
+    print(search)
